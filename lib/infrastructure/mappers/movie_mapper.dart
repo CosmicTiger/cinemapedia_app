79474c868dda1a6c85bb2d64bb2ffa917e77d874
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-18 19:24:13 -0600

    ✨ feature: add movie_mapper for datasource compatibility layer

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:cinemapedia_app/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  /// NOTE: This class is in charge to interpret and translate from responses models that comes from our datasources
+  /// towards our domain entities.
+  static Movie movieDBToEntity(MovieFromMovieDB movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+          : 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=',
+      releaseDate: movieDB.releaseDate.toString(),
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+}
