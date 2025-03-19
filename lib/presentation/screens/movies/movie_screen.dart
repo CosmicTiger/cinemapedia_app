5432690a3670f069527b94c12c51b3de404cb358
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-19 13:21:59 -0600

    ✨ feature: add Deep Linking basis

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  static const name = 'movie_screen';
+
+  final String movieId;
+
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('MovieID: $movieId'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
+        ));
+  }
+}
--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia_app/config/helpers/human_formats.dart';
 import 'package:cinemapedia_app/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListView extends StatefulWidget {
   final List<Movie> movies;
@@ -90,7 +91,9 @@ class _Slide extends StatelessWidget {
                     width: 150,
                     loadingBuilder: (context, child, loadingProgress) {
                       if (loadingProgress == null) {
-                        return FadeIn(child: child);
+                        return GestureDetector(
+                            onTap: () => context.push('/movie/${movie.id}'),
+                            child: FadeIn(child: child));
                       }
 
                       return Padding(
