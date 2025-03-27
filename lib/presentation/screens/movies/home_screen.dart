e24c1af34435e57267475381c1b411c34e9edc11
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-27 05:22:09 -0600

    ✨ feature: add keep-alive feature on home_screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,23 +2,54 @@ import 'package:cinemapedia_app/presentation/views/views.dart';
 import 'package:cinemapedia_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const name = 'home_screen';
   final int pageIndex;
 
   const HomeScreen({super.key, required this.pageIndex});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  late PageController pageController;
+
   final viewRoutes = const <Widget>[
     HomeView(),
-    PopularView(), // Placeholder for the categories view
+    PopularView(),
     FavoritesView(),
   ];
 
   @override
+  void initState() {
+    super.initState();
+    pageController = PageController(keepPage: true);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+    if (pageController.hasClients) {
+      pageController.animateToPage(widget.pageIndex,
+          curve: Curves.easeOut, duration: const Duration(milliseconds: 200));
+    }
+
     return Scaffold(
-      body: IndexedStack(index: pageIndex, children: viewRoutes),
-      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: pageIndex),
+      body: PageView(controller: pageController, children: viewRoutes),
+      bottomNavigationBar:
+          CustomBottomNavigationBar(currentIndex: widget.pageIndex),
     );
   }
 }
