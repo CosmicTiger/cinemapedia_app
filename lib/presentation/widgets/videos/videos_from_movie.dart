266edffd99e021c8181792b3200fc6cf6ce208fc
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-27 04:59:06 -0600

    ✨ feature: add youtube videos implementation

--- lib/presentation/widgets/videos/videos_from_movie.dart
+++ lib/presentation/widgets/videos/videos_from_movie.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia_app/presentation/providers/providers.dart';
 import 'package:cinemapedia_app/domain/entities/entities.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 final FutureProviderFamily<List<Video>, int> videosFromMovieProvider =
     FutureProvider.family((ref, int movieId) {
@@ -20,20 +21,100 @@ class VideosFromMovie extends ConsumerWidget {
     final moviesFromVideo = ref.watch(videosFromMovieProvider(movieId));
 
     return moviesFromVideo.when(
-      data: (videos) => Container(
-          color: Colors.red,
-          margin: const EdgeInsetsDirectional.only(bottom: 50),
-          child: SizedBox(
-            height: 350,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: videos.map((e) => Text(e.name)).toList(),
-            ),
-          )),
+      data: (videos) => _VideosList(
+        videos: videos,
+      ),
       error: (_, __) => const Center(
           child: Text('No videos about the movie could be found 💀')),
       loading: () =>
           const Center(child: CircularProgressIndicator(strokeWidth: 2)),
     );
+  }
+}
+
+class _VideosList extends StatelessWidget {
+  final List<Video> videos;
+
+  const _VideosList({
+    required this.videos,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsetsDirectional.only(bottom: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Videos',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ...videos.map((video) => _YouTubeVideoPlayer(
+                youtubeId: videos.first.youtubeKey, name: video.name)),
+          ],
+        ));
+  }
+}
+
+class _YouTubeVideoPlayer extends StatefulWidget {
+  final String youtubeId;
+  final String name;
+
+  const _YouTubeVideoPlayer({
+    required this.youtubeId,
+    required this.name,
+  });
+
+  @override
+  State<_YouTubeVideoPlayer> createState() => _YouTubeVideoPlayerState();
+}
+
+class _YouTubeVideoPlayerState extends State<_YouTubeVideoPlayer> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.youtubeId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: false,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: YoutubePlayerBuilder(
+            player: YoutubePlayer(controller: _controller),
+            builder: (context, player) {
+              return Column(
+                children: [
+                  player,
+                  Text(
+                    widget.name,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              );
+            }));
   }
 }
