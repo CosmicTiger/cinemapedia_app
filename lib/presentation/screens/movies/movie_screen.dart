b2533ff0129d84d6e4f9a1c93b79ce3043846413
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-27 03:06:34 -0600

    💄 ui: improve movie screen title in flexibleSpaceBar

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -192,7 +192,13 @@ class _CustomSliverAppBar extends ConsumerWidget {
         )
       ],
       flexibleSpace: FlexibleSpaceBar(
-          titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          titlePadding: const EdgeInsets.only(bottom: 0),
+          title: _CustomGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.7, 1.0],
+            colors: [Colors.transparent, scaffoldBackgroundColor],
+          ),
           background: Stack(
             children: [
               SizedBox.expand(
